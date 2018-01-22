9a2068e2f9b19fe68c75ac98dcab500bc288e1e6
Peter Williams <pwilliams@astro.ucla.edu>
2018-01-22 08:56:42 -0800

    Updating to newest version

--- code/functions.py
+++ code/functions.py
@@ -1,60 +1,272 @@
 import numpy as np
 from scipy.interpolate import interp1d
+from PyAstronomy import pyasl
+import matplotlib.pyplot as plt
+import time
 
-def hermitePolynomial(x,h):
-	return h[0] + h[1]*np.sqrt(2)*x + h[2]/np.sqrt(2)*(2.*x**2 - 1.) + \
+def log_rebin(lamRange, spec, oversample=False, velscale=None, flux=False):
+    """
+    From https://github.com/moustakas/impy/blob/master/lib/ppxf/ppxf_util.py
+
+    Logarithmically rebin a spectrum, while rigorously conserving the flux.
+    Basically the photons in the spectrum are simply redistributed according
+    to a new grid of pixels, with non-uniform size in the spectral direction.
+    
+    When the flux keyword is set, this program performs an exact integration 
+    of the original spectrum, assumed to be a step function within the 
+    linearly-spaced pixels, onto the new logarithmically-spaced pixels. 
+    The output was tested to agree with the analytic solution.
+    """
+    lamRange = np.asarray(lamRange)
+    if len(lamRange) != 2:
+        raise ValueError('lamRange must contain two elements')
+    if lamRange[0] >= lamRange[1]:
+        raise ValueError('It must be lamRange[0] < lamRange[1]')
+    s = spec.shape
+    if len(s) != 1:
+        raise ValueError('input spectrum must be a vector')
+    n = s[0]
+    if oversample:
+        m = int(n*oversample)
+    else:
+        m = int(n)
+
+    dLam = np.diff(lamRange)/(n - 1.)        # Assume constant dLam
+    lim = lamRange/dLam + [-0.5, 0.5]        # All in units of dLam
+    borders = np.linspace(*lim, num=n+1)     # Linearly
+    logLim = np.log(lim)
+
+    c = 299792.458                           # Speed of light in km/s
+    if velscale is None:                     # Velocity scale is set by user
+        velscale = np.diff(logLim)/m*c       # Only for output
+    else:
+        logScale = velscale/c
+        m = int(np.diff(logLim)/logScale)    # Number of output pixels
+        logLim[1] = logLim[0] + m*logScale
+
+    newBorders = np.exp(np.linspace(*logLim, num=m+1)) # Logarithmically
+    k = (newBorders - lim[0]).clip(0, n-1).astype(int)
+
+    specNew = np.add.reduceat(spec, k)[:-1]  # Do analytic integral
+    specNew *= np.diff(k) > 0    # fix for design flaw of reduceat()
+    specNew += np.diff((newBorders - borders[k])*spec[k])
+
+    if not flux:
+        specNew /= np.diff(newBorders)
+
+    # Output log(wavelength): log of geometric mean
+    logLam = np.log(np.sqrt(newBorders[1:]*newBorders[:-1])*dLam)
+
+    return specNew, logLam, velscale
+
+def gaussian_filter1d(spec, sig):
+    """
+    From https://github.com/moustakas/impy/blob/master/lib/ppxf/ppxf_util.py
+
+    Convolve a spectrum by a Gaussian with different sigma for every
+    pixel, given by the vector "sigma" with the same size as "spec".
+    If all sigma are the same this routine produces the same output as
+    scipy.ndimage.gaussian_filter1d, except for the border treatment.
+    Here the first/last p pixels are filled with zeros.
+    When creating  template library for SDSS data, this implementation
+    is 60x faster than the naive loop over pixels.
+    """
+
+    sig = sig.clip(0.01)  # forces zero sigmas to have 0.01 pixels
+    p = int(np.ceil(np.max(3*sig)))
+    m = 2*p + 1  # kernel size
+    x2 = np.linspace(-p, p, m)**2
+
+    n = spec.size
+    a = np.zeros((m, n))
+    for j in range(m):   # Loop over the small size of the kernel
+        a[j, p:-p] = spec[j:n-m+j+1]
+
+    gau = np.exp(-x2[:, None]/(2*sig**2))
+    gau /= np.sum(gau, 0)[None, :]  # Normalize kernel
+
+    conv_spectrum = np.sum(a*gau, 0)
+
+    return conv_spectrum
+
+def convolveGauss(x,y,sig,pad):
+	conv = np.zeros(len(x))
+	dx = x[1]-x[0]
+	xgauss = np.arange(-dx*pad,dx*pad,dx)
+	ygauss = Gauss().func(xgauss,(1.,0.,sig))
+
+	for i in range(len(x)):
+		l0 = x[i]
+		minsum = max(0,i-pad)
+		maxsum = min(len(x),i+pad)
+		xx = np.array([x[j] for j in range(minsum,maxsum)])
+		yy = np.array([y[j] * ygauss[j-i+pad] for j in range(minsum,maxsum)])
+		gaussian_trimmed = np.array([ygauss[j-i+pad] for j in range(minsum,maxsum)])
+		conv[i] = np.sum(yy)/np.sum(gaussian_trimmed)
+	return conv
+
+def logBroaden(x,dv):
+	res=2.
+	intres=10
+	
+	# First re-bin to a log-linear wavelength scale
+	#t0 = time.time()
+	Flogl,logl,velscale = log_rebin([x[0,0],x[-1,0]],x[:,1])
+	#print "log_rebin: ",time.time()-t0
+
+	# Do the broadening
+	#t0 = time.time()
+	#dv = 100.
+	Floglbroad = gaussian_filter1d(Flogl,dv/velscale)
+	#print "Broadening: ",time.time()-t0
+	Flogl = Floglbroad
+
+	#plt.figure()
+	#plt.plot(logl[300:500],Flogl[300:500])
+	#plt.plot(logl[300:500],Floglbroad[300:500])
+	#plt.show()
+	#exit()
+
+	# Re-bin back to linear wavelength scale
+	xinterp = interp1d(logl,Flogl)
+	l = x[:,0].copy()
+	Fl = np.zeros(len(l))
+	dl = (l[-1]-l[0])/len(l)
+	for i in range(1,len(l)-1):
+		xx = np.linspace(l[i]-dl/2.,l[i]+dl/2.,intres,endpoint=True)
+		logxx = np.log(xx)
+		logyy = xinterp(logxx)
+		Fl[i] = np.trapz(logyy,logxx)/(logxx[-1]-logxx[0])
+	
+	return np.vstack([l,Fl]).T
+
+class GaussHermite:
+	def __init__(self):
+		#self.x = x
+		#self.theta = theta
+		self.nparms = 8
+
+	def __call__(self,x,theta):
+		return self.func(x,theta)
+	
+	def func(self,x,theta):
+		gamma, V, sigma, h0, h1, h2, h3, h4 = theta
+		h = [h0, h1, h2, h3, h4]
+		w = (x - V)/sigma
+		gausspart = 1./np.sqrt(2.*np.pi)*np.exp(-0.5*w**2)/sigma
+		return gamma * gausspart * self.hermitePolynomial(w,h)
+
+	def hermitePolynomial(self,x,h):
+		return h[0] + h[1]*np.sqrt(2)*x + h[2]/np.sqrt(2)*(2.*x**2 - 1.) + \
 			h[3]/np.sqrt(6)*(2.*np.sqrt(2)*x**3 - 3.*np.sqrt(2)*x) + \
 			h[4]/np.sqrt(24)*(4.*x**4 - 12.*x**2 + 3.)
 
-def gaussHermite(x,theta):
-	gamma, V, sigma, h0, h1, h2, h3, h4 = theta
-	h = [h0, h1, h2, h3,h4]
-	w = (x - V)/sigma
-	gausspart = 1./np.sqrt(2.*np.pi)*np.exp(-0.5*w**2)/sigma
-	return gamma * gausspart * hermitePolynomial(w,h)
+class Gauss:
+	def __init__(self):
+		#self.x = x
+		#self.theta = theta
+		self.nparms = 3
 
-def gauss(x,theta):
-	A, mu, sigma = theta
-	return A/(np.sqrt(2.*np.pi) * sigma) * np.exp(-0.5 * ((x - mu)/sigma)**2)
+	def __call__(self,x,theta):
+		return self.func(x,theta)
+	
+	def func(self,x,theta):
+		A, mu, sigma = theta
+		return A/(np.sqrt(2.*np.pi) * sigma) * np.exp(-0.5 * ((x - mu)/sigma)**2)
 
-def offset(x,theta):
-	offset = theta
-	return x*0+offset
+class Offset:
+	def __init__(self):
+		self.nparms = 1
 
-def powerLaw(x,theta):
-	A, alpha = theta
-	return A * x**alpha / (5000**alpha)
+	def __call__(self,x,theta):
+		return self.func(x,theta)
+	
+	def func(self,x,theta):
+		offset = theta
+		return x*0+offset
 
-def starlight_interpolate():
-	# Bruzual & Charlot (2003) templates
-	bc03_dir = "../data/bc03_2016/templates/"
-	templatename = "ssp_11Gyr_z008.spec.gz"
-	template = np.loadtxt(bc03_dir+templatename)[300:6000]
-	template = np.vstack([np.vstack([np.array([0,0]),template]),np.array([8000,0])])
-	f_interp = interp1d(template[:,0],template[:,1])
-	return f_interp
+class PowerLaw:
+	def __init__(self):
+		#self.x = x
+		#self.theta = theta
+		self.nparms = 2
 
-def starlight(x,theta):
-	A, z = theta
-	return A * stars_interp(x/(1.+z))
+	def __call__(self,x,theta):
+		return self.func(x,theta)
+	
+	def func(self,x,theta):
+		A, alpha = theta
+		return A * x**alpha / (5000**alpha)
 
-def feII_interpolate():
-	feii_dir = "../data/FeII_template_4000_5500/700FeII/"
-	templatenames = ["fe_f.txt","fe_g.txt","fe_IZw1.txt","fe_p.txt","fe_s.txt"]
-	f_interp = [0]*len(templatenames)
-	for i in range(len(templatenames)):
-		template = np.loadtxt(feii_dir+templatenames[i])
+class Starlight:
+	def __init__(self):
+		self.nparms = 3
+		#self.dv = 100.
+		#self.stars_interp = self.starlight_interpolate(self.dv)
+		self.memoize = {"dv":{}}
+	def __call__(self,x,theta):
+		return self.func(x,theta)
+	
+	def func(self,x,theta):
+		A, z, dv = theta
+		if dv in self.memoize["dv"].keys():
+			self.stars_interp = self.memoize["dv"][dv]
+		else:
+			self.stars_interp = self.starlight_interpolate(dv)
+			self.memoize["dv"][dv] = self.stars_interp
+		return A * self.stars_interp(x/(1.+z))
+
+	def starlight_interpolate(self,dv):
+		# Bruzual & Charlot (2003) templates
+		bc03_dir = "../data/bc03_2016/templates/"
+		templatename = "ssp_11Gyr_z008.spec.gz"
+		template = np.loadtxt(bc03_dir+templatename)[1500:2800]
+
+		template = logBroaden(template,dv)
 		template = np.vstack([np.vstack([np.array([0,0]),template]),np.array([8000,0])])
-		f_interp[i] = interp1d(template[:,0],template[:,1])
-	return f_interp
+		f_interp = interp1d(template[:,0],template[:,1])
+		return f_interp
 
-def feII(x,theta):
-	A1,A2,A3,A4,A5,z = theta
-	A = [A1,A2,A3,A4,A5]
-	return sum(A[i] * feii_interp[i](x/(1.+z)) for i in range(len(A)))
+class FeII:
+	def __init__(self):
+		self.nparms = 6
+		self.dv = 100.
+		self.feii_interp = self.starlight_interpolate(self.dv/3.e5)
 
-stars_interp = starlight_interpolate()
-feii_interp = feII_interpolate()
+	def __call__(self,x,theta):
+		return self.func(x,theta)
+	
+	def func(self,x,theta):
+		A1,A2,A3,A4,A5,z = theta
+		A = [A1,A2,A3,A4,A5]
+		return sum(A[i] * self.feii_interp[i](x/(1.+z)) for i in range(len(A)))
+
+	def feII_interpolate(self,dv):
+		feii_dir = "../data/FeII_template_4000_5500/700FeII/"
+		templatenames = ["fe_f.txt","fe_g.txt","fe_IZw1.txt","fe_p.txt","fe_s.txt"]
+		f_interp = [0]*len(templatenames)
+		for i in range(len(templatenames)):
+			template = np.loadtxt(feii_dir+templatenames[i])
+			#template = logBroaden(template,dv)
+			template = np.vstack([np.vstack([np.array([0,0]),template]),np.array([8000,0])])
+			f_interp[i] = interp1d(template[:,0],template[:,1])
+		return f_interp
+
+def testing():
+	testclass = eval("Starlight()")
+	print testclass
+	x = np.arange(4700,5300,1)
+	theta = [0.7,0.0417,100.]
+	f = testclass.func
+	print f
+	print f(x,theta)
+
+if __name__=="__main__":
+	testing()
+
+
+
+
 
 
 
--- code/mpfitDecomp.py
+++ code/mpfitDecomp.py
@@ -1,3 +1,4 @@
+from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.io.fits as fits
@@ -5,17 +6,25 @@ from astropy import units as u
 import scipy.optimize as op
 from scipy.interpolate import interp1d
 import mpyfit
-import config
 from functions import *
 import time
+import sys
+import re
 
-def prior(theta, priors):
-	for i in range(len(theta)):
-		if not priors[i][0] < theta[i] < priors[i][1]:
-			return np.inf
-	return 0.0
+rcParams['text.usetex']=True
+rcParams['text.latex.unicode']=True
 
-def lnlike(theta,args):
+class Component():
+	def __init__(self,funcname):
+		self.func = eval(funcname+"()")
+		self.nparms = self.func.nparms
+		self.index = None
+		self.params = None
+		self.prior = None
+		self.texname = None
+		self.fixed = [False]*self.nparms
+
+def deviates(theta,args):
 	x, y, func_list, p0_len_list = args
 	p0 = [0]*len(p0_len_list)
 	for i in range(len(p0)):
@@ -23,53 +32,288 @@ def lnlike(theta,args):
 		p0[i] = theta[indexstart:indexstart+p0_len_list[i]]
 	
 	model = sum(func_list[i](x,p0[i]) for i in range(len(func_list)))
+	deviates = y - model
 	
-	return model - y
-
-def lnprob(theta, args):
-	x, y, func_list, priors, p0_len_list = args
-	lp = prior(theta,priors)
-	deviates = lp + lnlike(theta, (x, y, func_list,p0_len_list))
-
 	return deviates
 
-if __name__ == "__main__":
-	datadir = "../data/testspectrum/"
-	specname = "testspectrum.txt"
-	x = np.loadtxt(datadir+specname)
-	x = x[500:1300]
+def readConfigFile(configfile):
+	with open(configfile,'r') as f:
+		lines = f.read().splitlines() 
+	linenumbers = []
+	startfile = None
+	l = 0
+	# Go through lines one by one and execute commands in order
+	while l < len(lines):
+		if re.match("specloc",lines[l]):
+			specloc = lines[l].split()[1]
+			x = np.loadtxt(specloc)
+			x = x[500:1300]
+			print "Loaded spectrum from %s" % specloc
 
-	t0 = time.time()
+		if re.match("funcfile",lines[l]):
+			funcfile = lines[l].split()[1]
+			# Get the functions from funcfile
+			with open(funcfile,'r') as f:
+				funcs = f.read().splitlines()
+			for j in range(len(funcs)):
+				if re.match("^[0-99]\)",funcs[j]):
+					linenumbers.append(j)
+			print "Loaded list of available functions from %s" % funcfile
 
-	components = config.returnComponents()
+		if re.match("constraintfile",lines[l]):
+			constraintfile = lines[l].split()[1]
+			print "Loaded constraints from %s" % constraintfile
 
-	key_list = components.keys()
-	func_list = [components[key][0] for key in key_list]
-	p0_list = [components[key][1] for key in key_list]
-	p0_len_list = [len(components[key][1]) for key in key_list]
-	prior_list = [components[key][2] for key in key_list]
+		if re.match("startfile",lines[l]):
+			startfile = lines[l].split()[1]
+			print "Using the startfile %s" % startfile
+
+		if re.match("fit",lines[l]) or re.match("grid",lines[l]):
+			if re.match("fit",lines[l]):
+				funcnumbers = lines[l].split()[1].split(',')
+				print "Starting a fitting sequence with functions ", funcnumbers
+			elif re.match("grid",lines[l]):
+				funcnumbers = lines[l].split()[1].split(',')
+				gridfuncs = lines[l+1].split(',')
+				gridparms = map(int,lines[l+2].split(','))
+				print "Starting a grid search sequence with functions ", funcnumbers
+				print "Doing grid search on functions ", gridfuncs, " parameters ",gridparms
+				gridrange = map(
+					lambda x:map(float,x.split(',')),
+					lines[l+3].split(';')
+					)
+			
+			# Go through following lines to lead in any extra commands/options
+			t=1
+			fixed, plotting, outfile = [], False, None
+			while t > 0:
+				if re.match("fixed",lines[l+t]):
+					tmp = lines[l+t].split()[1]
+					fixed = np.array(map(
+						lambda x:map(int,x.split(',')),
+						tmp.split(';')
+						))
+					print "Fixing parameters:"
+					for i in range(len(fixed)):
+						print "  Function %i, parameter %i" % (fixed[i,0],fixed[i,1])
+				if re.match("outfile",lines[l+t]):
+					outfile = lines[l+t].split()[1]
+					print "Saving results to %s" % outfile
+				if re.match("startfile",lines[l+t]):
+					startfile = lines[l+t].split()[1]
+					print "Using the startfile %s" % startfile
+				if re.match("plotfit",lines[l+t]):
+					plotting = True
+				if re.match("end",lines[l+t]):
+					t=-1e9
+				t+=1
+
+			components = {}
+			for j in linenumbers:
+				if funcs[j][:-1] in funcnumbers:
+					funcname = funcs[j+2]
+					paramguess = map(float,funcs[j+3].split(','))
+					paramlims = map(
+						lambda x:map(float,x.split(',')),
+						funcs[j+4].split(';')
+						)
+					texname = funcs[j+5]
+					strname = funcs[j+1]
+					
+					index = funcs[j][:-1]
+					components[index] = Component(funcname)
+					components[index].params = paramguess
+					components[index].prior = paramlims
+					components[index].texname = texname
+					components[index].name = strname
+					for i in range(len(fixed)):
+						if index == str(fixed[i,0]):
+							components[index].fixed[fixed[i,1]] = True
+
+			if re.match("fit",lines[l]):
+				pfit,results = executeFit(components,startfile,x)
+			elif re.match("grid",lines[l]):
+				pfit,results = gridSearch(components,startfile,x,gridfuncs,gridparms,gridrange)
+
+			if outfile != None:
+				saveStartFile(outfile,pfit)
+			
+			for key in funcnumbers:
+				print components[key].name+": ",pfit[key]
+			if plotting:
+				plotFit(x,components,pfit,saveloc=False)
+
+		if re.match("quit",lines[l]):
+			l = 1e9
+		l += 1
+		
+	return pfit
+
+def readStartFile(startfile,components):
+	if startfile == None:
+		return components
+	print "Using the startfile %s" % startfile
+	print "Loading parameters: "
+	with open(startfile,'r') as f:
+		for l in f.readlines():
+			pars = l.split(',')
+			component_name = pars[0]
+			print component_name
+			components[component_name].params = map(float,pars[1:])
+	return components
+
+def saveStartFile(outfile,pfit):
+	with open(outfile,'w') as f:
+		for key in sort(pfit.keys()):
+			f.write(key+",")
+			f.write(",".join(map(str,pfit[key])))
+			f.write("\n")
+
+def executeFit(components,startfile,x):
+	components = readStartFile(startfile,components)
+	keys = sort(components.keys())
+
+	pfit,results = mpfitOptimize(deviates,x,components)
+	return pfit,results
+
+def gridSearch(components,startfile,x,gridfuncs,gridparms,gridrange):
+	Ndims = len(gridfuncs)
+
+	components = readStartFile(startfile,components)
+	keys = sort(components.keys())
+
+	dims = [int(gridrange[i][2]) for i in range(Ndims)]
+	ax = [0]*Ndims
+	for i in range(Ndims):
+		ax[i] = np.linspace(gridrange[i][0],gridrange[i][1],gridrange[i][2])
+	mesh = np.meshgrid(*ax)
+
+	gridresults = []
+	for j in range(len(mesh[0].ravel())):
+		for i in range(Ndims):
+			components[gridfuncs[i]].params[gridparms[i]] = mesh[i].ravel()[j]
+		pfit,results = mpfitOptimize(deviates,x,components)
+	
+		print "chi^2: ",results['orignorm']," --> ",results['bestnorm']
+		gridresults.append([results['bestnorm'],pfit,results])
+
+	gridresults = np.array(gridresults)
+	bestrun = argmin(gridresults[:,0])
+	
+	return gridresults[bestrun,1],gridresults[bestrun,2]
+
+def mpfitOptimize(deviates,x,components):
+	keys = sort(components.keys())
+	func_list = [components[key].func for key in keys]
+	p0_list = [components[key].params for key in keys]
+	p0_len_list = [len(components[key].params) for key in keys]
+	prior_list = [components[key].prior for key in keys]
+	fixed_list = [components[key].fixed for key in keys]
 
 	p0 = reduce(lambda x,y:x+y,p0_list)
 	priors = reduce(lambda x,y:x+y,prior_list)
+	fixed = reduce(lambda x,y:x+y,fixed_list)
+	limited = [[True,True]]*len(p0)
+
+	tied = [False]*len(p0)
+	tied[19] = "3 * p[16]"
+	
+	parinfo = [{} for i in range(len(p0))]
+	for i in range(len(p0)):
+		parinfo[i]['limited'] = tuple(limited[i])
+		parinfo[i]['limits'] = tuple(priors[i])
+		parinfo[i]['fixed'] = fixed[i]
+		parinfo[i]['tied'] = tied[i]
 
 	# Do the fitting
 	print "Fitting using mpyfit..."
-	pfit, results = mpyfit.fit(lnprob, p0, (x[:,0],x[:,1],func_list,priors,p0_len_list))
-	theta_best = pfit
+	pfit, results = mpyfit.fit(deviates, p0, (x[:,0],x[:,1],func_list,p0_len_list),parinfo=parinfo)
 
-	theta = [0]*len(func_list)
-	for i in range(len(theta)):
-		indexstart = sum(p0_len_list[i] for i in range(i))
-		theta[i] = theta_best[indexstart:indexstart+p0_len_list[i]]
-	for i in range(len(key_list)):
-		print key_list[i], theta[i]
+	# Put back into dictionary form
+	pbest = {}
+	for j in range(len(keys)):
+		pbest[keys[j]] = pfit[sum(p0_len_list[k] for k in range(j)):sum(p0_len_list[k] for k in range(j+1))]
+	return pbest,results
 
+def plotFit(x,components,pfit,saveloc,showtag=True):
+	keys = sort(components.keys())
+	func_list = [components[key].func for key in keys]
+	theta = [pfit[key] for key in keys]
+	tex_list = [components[key].texname for key in keys]
+	# Plotting values
+	figwidth = 13.
+	figheight = 6.
+
+	_label_fontparams = {'fontsize':14}
+	_axis_fontparams = {'fontsize':14}
+	_tick_fontparams = {'fontsize':10}
+
+	_lpadin = 0.9
+	_rpadin = 0.1
+	_bpadin = 0.5
+	_tpadin = 0.1
+	_wpadin = 0.0
+	_hpadin = 0.0
+
+	_lpad = _lpadin/figwidth
+	_rpad = _rpadin/figwidth
+	_hpad = _hpadin/figheight
+	_tpad = _tpadin/figheight
+	_bpad = _bpadin/figheight
+	_wpad = _wpadin/figwidth
+
+	_spheight_t = 0.7
+	_spheight_b = 1. - _spheight_t - _bpad - _tpad - _hpad
+	_spwidth = 1. - _lpad - _rpad
+
+	plt.figure(1,figsize=(figwidth,figheight))
+	ax1 = plt.axes((
+		_lpad,
+		1. - _tpad - _spheight_t,
+		_spwidth,
+		_spheight_t
+		))
 	plt.plot(x[:,0],x[:,1],'k')
-	plt.plot(x[:,0],sum(func_list[i](x[:,0],theta[i]) for i in range(len(func_list))))
+	plt.plot(
+		x[:,0],
+		sum(func_list[i](x[:,0],theta[i]) for i in range(len(func_list))),
+		color='b',
+		label="${\\rm Full~model}$"
+		)
 	#plt.plot(x[:,0],150.*x[:,0]**-0.5)
 	for i in range(len(func_list)):
-		plt.plot(x[:,0],func_list[i](x[:,0],theta[i]))
-	plt.show()
+		plt.plot(x[:,0],func_list[i](x[:,0],theta[i]),label=tex_list[i])
+	#plt.xlabel("${\\rm Wavelength~(\\AA)}$")
+	plt.ylim(-1,int(max(x[:,1]))+1)
+	plt.yticks(range(int(max(x[:,1]))+2))
+	ax1.set_xticklabels("")
+	plt.ylabel("${\\rm Flux~(arbitrary)}$",**_label_fontparams)
+	plt.legend(loc='upper left',ncol=2,frameon=False)
+
+	ax2 = plt.axes((
+		_lpad,
+		1. - _tpad - _spheight_t - _hpad - _spheight_b,
+		_spwidth,
+		_spheight_b
+		))
+	plt.plot(x[:,0],(sum(func_list[i](x[:,0],theta[i]) for i in range(len(func_list))) - x[:,1])/x[:,1],'k')
+	plt.axhline(0,ls="--",color='k')
+	plt.xlabel("${\\rm Wavelength~(\\AA)}$",**_label_fontparams)
+	plt.ylabel("$({\\rm Model} - {\\rm Data})/{\\rm Data}$",**_label_fontparams)
+	plt.ylim(-0.17,0.17)
+	if saveloc != False:
+		plt.savefig(saveloc)
+	if showtag:
+		plt.show()
+
+def main():
+	configfile = sys.argv[1]
+	outfile = configfile.split('.')[0]+'.out'
+	pfit = readConfigFile(configfile)
+	saveStartFile(outfile,pfit)
+
+if __name__ == "__main__":
+	main()
 
 
 
